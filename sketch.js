2950bde816497da04a6e70c1a110575e38b72efc
Breno Santos <brenosantos145@gmail.com>
2024-09-05 22:56:58 -0300

    feat: add elevation to the terrain

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 let cols, rows;
 let scl = 20;
+let terrain = [];
+
+let w, h;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
 
-  cols = width / scl;
-  rows = height / scl;
+  w = width * 4;
+  h = height * 3;
+  cols = w / scl;
+  rows = h / scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = random(-10, 10);
+    }
+  }
 }
 
 function draw() {
@@ -13,14 +25,15 @@ function draw() {
   stroke(255);
   noFill();
 
+  translate(width / 2, height / 2);
   rotateX(PI / 3);
-  translate(-width / 2, -height / 2);
+  translate(-w / 2, -h / 2);
 
   for (let y = 0; y < cols; y++) {
     beginShape(TRIANGLE_STRIP);
-    for (let x = 0; x < rows; x++) {
-      vertex(x * scl, y * scl);
-      vertex(x * scl, (y + 1) * scl);
+    for (let x = 0; x < rows - 1; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
     }
     endShape();
   }
