c3bddc0984d90d6d597adff49735c934b094ec8e
Breno Santos <brenosantos145@gmail.com>
2024-09-05 22:13:33 -0300

    feat: first render

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let cols, rows;
+let scl = 20;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 400, WEBGL);
+
+  cols = width / scl;
+  rows = height / scl;
 }
 
 function draw() {
   background(0);
+
+  translate(-width / 2, -height / 2);
+
+  for (let x = 0; x < rows; x++) {
+    for (let y = 0; y < cols; y++) {
+      stroke(255);
+      noFill();
+      rect(x * scl, y * scl, scl, scl);
+    }
+  }
 }
